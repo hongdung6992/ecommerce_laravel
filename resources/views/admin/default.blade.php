08e50c1dd022dcc05527363816debcdcaf0fbf97
Hong Dung Nguyen <hongdung6992@gmail.com>
2019-08-02 22:19:59 +0700

    fixed mix

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,42 +7,28 @@
 	<meta name="author" content="Pike Web Development - https://www.pikephp.com">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title')</title>
+	<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 	<link rel="stylesheet" href="{{ mix('css/all.css') }}">
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
-	<!-- Favicon -->
-	<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
-
+	<script src="{{ mix('js/all.js') }}" defer></script>
+	<script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body class="adminbody">
-
 	<div id="main">
 
 		<!-- top bar navigation -->
 		@include('admin.layout.topbar')
-		<!-- End Navigation -->
-
 
 		<!-- Left Sidebar -->
 		@include('admin.layout.sidebar')
-		<!-- End Sidebar -->
-
 
 		<div class="content-page">
-
-			<!-- Start content -->
-				@yield('content')
-			<!-- END content -->
-
+			@yield('content')
 		</div>
-		<!-- END content-page -->
 
 		@include('admin.layout.footer')
 
 	</div>
-
-	<script src="{{ mix('js/all.js') }}"></script>
-	<script src="{{ mix('js/app.js') }}"></script>
 </body>
-
 </html>
